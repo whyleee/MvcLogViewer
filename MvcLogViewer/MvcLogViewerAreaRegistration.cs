b3afa9ecf75c1d04a2b595110e92af5dbe33a3c9
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2015-08-11 17:58:00 +0300

    Add area registration

---
+++ MvcLogViewer/MvcLogViewerAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MvcLogViewer
+{
+    public class MvcLogViewerAreaRegistration : AreaRegistration
+    {
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                name: "MvcLogViewer_Default",
+                url: "logs/{log}/{action}",
+                defaults: new { controller = "Log", action = "Index", log = UrlParameter.Optional }
+            );
+        }
+
+        public override string AreaName
+        {
+            get { return "MvcLogViewer"; }
+        }
+    }
+}
