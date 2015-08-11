5db89fc5594d18f8b6a41b4439da2bc647d20651
Pavel Nezhencev <pavel.nezhencev@creuna.com>
2015-08-11 17:53:15 +0300

    Create log viewer

---
+++ MvcLogViewer/Controllers/LogController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web.Mvc;
+using MvcLogViewer.Models;
+
+namespace MvcLogViewer.Controllers
+{
+    [Authorize]
+    public class LogController : Controller
+    {
+        private readonly string _logDir;
+
+        public LogController()
+        {
+            _logDir = ConfigurationManager.AppSettings["LogViewer.LogDir"] ?? "~/App_Data/logs/";
+
+            if (_logDir.StartsWith("~/") || _logDir.StartsWith("/"))
+            {
+                _logDir = Server.MapPath(_logDir);
+            }
+        }
+
+        public ActionResult Index(string log)
+        {
+            if (!string.IsNullOrEmpty(log))
+            {
+                return File("/logs/" + log, "text/plain"); // TODO: remove hardcode
+            }
+
+            var logs = Directory.GetFiles(_logDir)
+                .Where(path => path.EndsWith(".log"))
+                .Select(path => new FileInfo(path))
+                .Select(file => new LogFileModel
+                {
+                    Name = file.Name,
+                    Url = "/logs/" + file.Name,
+                    Size = GetFriendlyFileSize(file.Length)
+                })
+                .ToList();
+
+            CorrectSortOrder(logs);
+
+            return View(logs);
+        }
+
+        public ActionResult Delete(string log)
+        {
+            var logPath = Path.Combine(_logDir, log);
+            System.IO.File.Delete(logPath);
+
+            return Redirect("/logs");
+        }
+
+        private string GetFriendlyFileSize(long lengthInBytes)
+        {
+            var kb = Math.Round(lengthInBytes / 1024d);
+            var groupSeparator = NumberFormatInfo.CurrentInfo.NumberGroupSeparator;
+            var friendly = kb.ToString("N0").Replace(groupSeparator, " ") + " KB";
+
+            return friendly;
+        }
+
+        private void CorrectSortOrder(List<LogFileModel> logs)
+        {
+            if (logs.Any())
+            {
+                var latestLog = logs.First();
+                logs.Remove(latestLog);
+                logs.Reverse();
+                logs.Insert(0, latestLog);
+            }
+        }
+    }
+}
---
+++ MvcLogViewer/Models/LogFileModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MvcLogViewer.Models
+{
+    public class LogFileModel
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Size { get; set; }
+    }
+}
